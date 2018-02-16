39da05ef2282b574fa9bc2bea1bf20379ac39eaf
hellgateL <hello_kitties@outlook.com>
2018-02-16 14:00:00 +0100

    init webpack config

---
+++ webpack.config.babel.js
@@ -0,0 +1,25 @@
+import path from 'path';
+
+const library_name = 'libphonenumber-getexample';
+const global_variable_name = 'libphonenumber';
+
+export default {
+    entry: path.join(__dirname, 'src/index.js'),
+    devtool: 'source-map',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: `${library_name}.min.js`,
+        library: global_variable_name,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.js)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
